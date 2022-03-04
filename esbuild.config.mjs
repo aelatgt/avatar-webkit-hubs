0b4ac88bee72b98077f0f6e80b64bb231fc067d8
mattrossman <rossman@gatech.edu>
2022-03-04 11:04:44 -0500

    Configure .env.local

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,5 +1,9 @@
 import esbuild from "esbuild"
 import postcss from "esbuild-postcss"
+import dotenv from "dotenv"
+
+const env = dotenv.config({ path: ".env.local" })
+const defineStrings = Object.fromEntries(Object.entries(env.parsed).map(([key, value]) => [key, `\"${value}\"`]))
 
 /** @type {esbuild.BuildOptions} */
 export const buildOptions = {
@@ -9,6 +13,7 @@ export const buildOptions = {
   format: "esm",
   target: "esnext",
   plugins: [postcss()],
+  define: defineStrings,
   jsxFactory: "h",
   jsxFragment: "Fragment",
   inject: ["src/preact-shim.js"],
