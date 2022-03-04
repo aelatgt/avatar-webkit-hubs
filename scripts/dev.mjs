46f1fe3488b3a3992b26719f5e195c02764b99f4
mattrossman <rossman@gatech.edu>
2022-03-04 11:20:41 -0500

    Log dev server requests

--- scripts/dev.mjs
+++ scripts/dev.mjs
@@ -4,6 +4,9 @@ import { buildOptions } from "../esbuild.config.mjs"
 /** @type {esbuild.ServeOptions} */
 const serveOptions = {
   port: 3000,
+  onRequest: (args) => {
+    console.log(`[${args.status}] ${args.method} ${args.path}: completed in ${args.timeInMS} ms`)
+  },
 }
 
 const serveResult = await esbuild.serve(serveOptions, {
