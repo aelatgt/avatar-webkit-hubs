da2fb71a1b2e7f6d36540adc5f5f906b9b0bf58a
mattrossman <rossman@gatech.edu>
2022-03-08 11:34:26 -0500

    Increase NAF updateRate

--- src/room.js
+++ src/room.js
@@ -9,4 +9,6 @@ import "./hubs-components/facetracking-widget"
 import "./hubs-components/avatar-webkit"
 import "./hubs-components/rpm-controller"
 
+NAF.options.updateRate = 30
+
 console.log("Custom script successfully executed")
